9eaaa6c81ac4e76e3125f3cc540ecefb8a1f02e0
Sogo Shirota <shirota.sogo@gmail.com>
2026-06-07 18:32:58 +0900

    Fix PWA home screen: relative start_url and restore service worker

--- web/sw.js
+++ web/sw.js
@@ -1,12 +1,18 @@
-// 旧キャッシュをすべて削除して自己解除するSW
-self.addEventListener('install', () => self.skipWaiting());
+const CACHE = 'health-note-v3';
+const FILES = ['./', './app.js', './manifest.json'];
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-      .then(() => clients.matchAll({ type: 'window' }))
-      .then(cs => cs.forEach(c => c.navigate(c.url)))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
